1cdff45a196f6bcbe1f0070aa05072c7a13b3930
gautamyogesh017 <gautamyogesh017@.github.com>
2023-05-11 20:51:17 +0545

    Client side of todoz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ViewTodoList } from "./components/viewTodo";
+import { CreateTodo } from "./components/createTodo";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-contents">
+      <h1>Hi </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" component={ViewTodoList} />
+          <Route path="/create-todo" component={CreateTodo} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
